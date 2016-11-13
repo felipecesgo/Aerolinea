d6ce3121fcf1a689ae328230e73cb37c4596a18e
Felipe <felipe9206@gmail.com>
2016-11-12 22:33:13 -0600

    he agregado la BD

---
+++ BaseDatos/Aerolinea.sql
@@ -0,0 +1,112 @@
+USE master;
+GO
+CREATE DATABASE Aerolinea;
+USE Aerolinea;
+
+CREATE TABLE Avion
+(
+    IdAvion int primary key  IDENTITY(1,1),
+	Matricula nvarchar (20) NOT NULL,
+    Marca nvarchar (20),
+	Modelo nvarchar (20)
+);
+
+CREATE TABLE Piloto
+(
+	IdPiloto int primary key  IDENTITY(1,1),
+    Cedula nvarchar (20) NOT NULL,
+    Nombre nvarchar (50) NOT NULL,
+    Apellido nvarchar (50) NOT NULL,
+    Telefono nvarchar(50),
+    Email nvarchar (20),
+    Residencia nvarchar (256),
+);
+
+CREATE TABLE Ruta
+(
+	IdRuta int primary key  IDENTITY(1,1),
+	Origen nvarchar (50) NOT NULL,
+	Destino nvarchar (50) NOT NULL,
+	Tarifa money NOT NULL,
+	Imagen image
+);
+
+CREATE TABLE Cliente
+(
+	IdCliente int primary key  IDENTITY(1,1),
+    Cedula nvarchar (25) NOT NULL,
+	Nombre nvarchar (50) NOT NULL,
+    Apellido nvarchar (50) NOT NULL,
+    Telefono nvarchar(50),
+    Email nvarchar (50),
+	Residencia nvarchar (256),
+	Usuario nvarchar (25) NOT NULL,
+    Contrasena nvarchar (256) NOT NULL
+);
+
+CREATE TABLE Vuelo
+(
+	IdVuelo int primary key  IDENTITY(1,1),
+	IdAvion int NOT NULL,
+	IdPiloto int NOT NULL,
+	IdRuta int NOT NULL,
+	CapacidadAsientos int NOT NULL,
+	EstadoVuelo nvarchar (25),
+    FechaSalida datetime NOT NULL,
+    Foreign key (IdAvion) REFERENCES Avion(IdAvion),
+    Foreign key (IdPiloto) REFERENCES Piloto(IdPiloto),
+	Foreign key (IdRuta) REFERENCES Ruta(IdRuta)
+);
+
+CREATE TABLE Rol
+(
+	IdRol int primary key  IDENTITY(1,1),
+	Nombre nvarchar (50) NOT NULL,
+	Descripcion nvarchar (256)
+);
+
+CREATE TABLE Agente
+(
+	IdAgente int primary key  IDENTITY(1,1),
+	IdRol int  NOT NULL,
+    Cedula nvarchar (25) NOT NULL,
+	Nombre nvarchar (50) NOT NULL,
+    Apellido nvarchar (50) NOT NULL,
+    Telefono nvarchar(50),
+    Email nvarchar (50),
+	Residencia nvarchar (256),
+	Usuario nvarchar (25)  NOT NULL,
+    Contrasena nvarchar (256)  NOT NULL,
+	Foreign key (IdRol) REFERENCES Rol(IdRol)
+);
+
+CREATE TABLE Tiquete
+(
+  IdTiquete int primary key  IDENTITY(1,1),
+  IdVuelo int  NOT NULL,
+  IdCliente int  NOT NULL,
+  NumeroDeAsiento int,
+  Fecha datetime NOT NULL DEFAULT GETDATE(),
+  Foreign key (IdVuelo) REFERENCES Vuelo(IdVuelo),
+  Foreign key (IdCliente) REFERENCES Cliente(IdCliente)
+)
+
+CREATE TABLE Queja
+(
+  IdQueja int primary key  IDENTITY(1,1),
+  IdVuelo int  NOT NULL , 
+  IdCliente  int  NOT NULL ,
+  Motivo nvarchar (256),
+  Descripcion nvarchar (256),
+  Foreign key (IdVuelo) REFERENCES Vuelo(IdVuelo),
+  Foreign key (IdCliente) REFERENCES Cliente(IdCliente)
+)
+
+CREATE TABLE Pago
+(
+	IdPago int primary key  IDENTITY(1,1),
+	IdTiquete int  NOT NULL,
+	FormaPago nvarchar (25)  NOT NULL , 
+	Monto MONEY NOT NULL,
+	Foreign key (IdTiquete) REFERENCES Tiquete(IdTiquete)
+)
